530003b9bee4db8e7517626d95286f85c5f5d434
newetijazz18 <newetijazz18@gmail.com>
2026-01-25 17:43:20 +0100

    Update googleplay.js

--- googleplay.js
+++ googleplay.js
@@ -5,8 +5,8 @@
         fh();}
     setTimeout('hh();', 500);
     function fh(){
-        location.href="https://www.effectivegatecpm.com/hc8aygtkdm?key=74aead4436dc8a8ff48822d11a78d7c8";
+        location.href="https://appliedjobszw.blogspot.com";
     }
     function goon(){
-        location.href="https://www.effectivegatecpm.com/hc8aygtkdm?key=74aead4436dc8a8ff48822d11a78d7c8";
+        location.href="https://appliedjobszw.blogspot.com";
     }
